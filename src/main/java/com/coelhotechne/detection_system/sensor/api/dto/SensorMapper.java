0485059bef9bae39dc8b780f26087816b1e0d29c
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-14 11:02:29 -0300

    created: SensorMapper version:v1 project:detection-system date: 14082026

---
+++ src/main/java/com/coelhotechne/detection_system/sensor/api/dto/SensorMapper.java
@@ -0,0 +1,44 @@
+package com.coelhotechne.detection_system.sensor.api.dto;
+
+
+import com.coelhotechne.detection_system.baseClass.GenericMapper;
+import com.coelhotechne.detection_system.sensor.domain.Sensor;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+@Component
+public class SensorMapper implements GenericMapper<Sensor, SensorResponse, SensorRequest> {
+
+    @Override
+    public Sensor toEntity(SensorRequest request) {
+        Objects.requireNonNull(request, "Request cannot be null");
+
+        Sensor entity = new Sensor();
+        entity.setName(request.name());
+        entity.setStatus(request.status());
+        entity.setActivationTime(request.activationTime());
+        entity.setMemoryUsed(request.memoryUsed());
+        entity.setDataTransferValue(request.dataTransferValue());
+        entity.setDataDescription(request.dataDescription());
+        return entity;
+    }
+
+    @Override
+    public SensorResponse toResponse(Sensor entity) {
+        Objects.requireNonNull(entity, "Entity cannot be null");
+
+        return new SensorResponse(
+                entity.getUuid(),
+                entity.getName(),
+                entity.isStatus(),
+                entity.getActivationTime(),
+                entity.getMemoryUsed(),
+                entity.getDataTransferValue(),
+                entity.getDataDescription(),
+                entity.getCreatedBy(),
+                entity.getLastModifiedBy(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt()
+        );
+    }
+}
