c1474799d47bc0f4704fb23e36fa8a84ea50617e
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-16 10:13:57 -0300

    created: DeviceMapper version:v1 project:detection-system date: 16082026

---
+++ src/main/java/com/coelhotechne/detection_system/device/api/dto/DeviceMapper.java
@@ -0,0 +1,39 @@
+package com.coelhotechne.detection_system.device.api.dto;
+
+import com.coelhotechne.detection_system.baseClass.GenericMapper;
+import com.coelhotechne.detection_system.device.domain.Device;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class DeviceMapper implements GenericMapper<Device,DeviceResponse,DeviceRequest> {
+    @Override
+    public Device toEntity(DeviceRequest request) {
+        Objects.requireNonNull(request,"Request cannot be null");
+        Device entity = new Device();
+        entity.setDeviceName(request.deviceName());
+        entity.setDeviceType(request.deviceType());
+        entity.setIpAdress(request.ipAddress());
+        entity.setStatus(request.status());
+        entity.setLastCommunication(request.lastCommunication());
+        entity.setAccessKey(request.accessKey());
+        return entity;
+    }
+
+    @Override
+    public DeviceResponse toResponse(Device entity) {
+        return new DeviceResponse(
+                entity.getUuid(),
+                entity.getCreatedBy(),
+                entity.getLastModifiedBy(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt(),
+                entity.getDeviceName(),
+                entity.getDeviceType(),
+                entity.getIpAdress(),
+                entity.getStatus(),
+                entity.getLastCommunication()
+        );
+    }
+}
