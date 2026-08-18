44bc0b80f9b8d1b84e1b08a0408bf863d7885023
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-18 09:59:26 -0300

    created: EventCategory version:v1 project:detection-system date: 18082026

---
+++ src/main/java/com/coelhotechne/detection_system/event/domain/enums/EventCategory.java
@@ -0,0 +1,12 @@
+package com.coelhotechne.detection_system.event.domain.enums;
+
+public enum EventCategory {
+    DETECTION,
+    STATE,
+    SYSTEM,
+    COMMUNICATION,
+    RESOURCE,
+    SECURITY,
+    ERROR,
+    UNKNOWN
+}
