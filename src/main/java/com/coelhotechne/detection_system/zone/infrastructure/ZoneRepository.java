18ba846cc6b3b0be21a17552095237cbec4592fa
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-19 16:36:11 -0300

    updated: ZoneRepository version:v1 project:detection-system date: 19082026

--- src/main/java/com/coelhotechne/detection_system/zone/infrastructure/ZoneRepository.java
+++ src/main/java/com/coelhotechne/detection_system/zone/infrastructure/ZoneRepository.java
@@ -1,4 +1,11 @@
 package com.coelhotechne.detection_system.zone.infrastructure;
 
-public interface ZoneRepository {
+import com.coelhotechne.detection_system.zone.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ZoneRepository extends JpaRepository<Zone, UUID> {
+    Optional<Zone>findById(UUID uuid);
 }
