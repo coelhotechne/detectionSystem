63996936262fbed632908894e4202332d3261909
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-20 21:36:46 -0300

    created: SensorConcurrentModificationException version:v1 project:detection-system date: 20082026

---
+++ src/main/java/com/coelhotechne/detection_system/sensor/exceptions/SensorConcurrentModificationException.java
@@ -0,0 +1,25 @@
+package com.coelhotechne.detection_system.sensor.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.ErrorResponseException;
+
+public class SensorConcurrentModificationException extends ErrorResponseException {
+    private final String sensorId;
+
+    public SensorConcurrentModificationException(String sensorId,Throwable cause){
+        super(HttpStatus.CONFLICT,buildProblemDetail(sensorId),cause);
+        this.sensorId=sensorId;
+    }
+
+
+    private static ProblemDetail buildProblemDetail(String sensorId){
+    ProblemDetail pd=ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT
+            ,"The sensor was modified by another request; please try again.");
+
+    pd.setTitle("Concurrent Modification");
+    pd.setProperty("sensorId",sensorId);
+    return pd;
+    }
+
+}
