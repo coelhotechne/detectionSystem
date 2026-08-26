88ae424f1be236524f0bef8b1fd7d0ffb05da6a7
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-26 19:41:52 -0300

    created: PowerSupplyStatus version:v1 project:detection-system date: 20082026

---
+++ src/main/java/com/coelhotechne/detection_system/batterySupply/domain/PowerSupplyStatus.java
@@ -0,0 +1,10 @@
+package com.coelhotechne.detection_system.batterySupply.domain;
+
+public enum PowerSupplyStatus {
+    NORMAL,
+    LOW,
+    CRITICAL,
+    CHARGING,
+    DISCONNECTED,
+    NOT_APPLICABLE
+}
