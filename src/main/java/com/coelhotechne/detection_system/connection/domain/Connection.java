9a305eb4d6ea23e2852c51a46b9e47bfc59c9301
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-28 10:49:28 -0300

    created: Connection version:v1 project:detection-system date: 28082026

---
+++ src/main/java/com/coelhotechne/detection_system/connection/domain/Connection.java
@@ -0,0 +1,15 @@
+package com.coelhotechne.detection_system.connection.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Connection {
+}
