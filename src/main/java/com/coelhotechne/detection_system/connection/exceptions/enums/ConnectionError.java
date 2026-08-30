7684dacb2b7e4e911b48df15666f8078ddb93b6f
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-08-30 13:14:48 -0300

    created: ConnectionError version:v1 project:detection-system date: 28082026

---
+++ src/main/java/com/coelhotechne/detection_system/connection/exceptions/enums/ConnectionError.java
@@ -0,0 +1,11 @@
+package com.coelhotechne.detection_system.connection.exceptions.enums;
+
+public enum ConnectionError {
+    UNSUPPORTED_PROTOCOL,
+    CONNECTION_FAILED,
+    TIMEOUT,
+    AUTHENTICATION_FAILED,
+    CONNECTION_REFUSED,
+    HOST_UNREACHABLE,
+    INVALID_CONFIGURATION
+}
