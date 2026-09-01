44563e4329c0556b8e66954c24ddf8f5760c1660
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-01 08:31:08 -0300

    updated: SensorRequest version:v1 project:detection-system date: 01092026

--- src/main/java/com/coelhotechne/detection_system/sensor/api/dto/SensorRequest.java
+++ src/main/java/com/coelhotechne/detection_system/sensor/api/dto/SensorRequest.java
@@ -1,17 +1,14 @@
 package com.coelhotechne.detection_system.sensor.api.dto;
 
+import com.coelhotechne.detection_system.batterysupply.domain.PowerSupply;
+import com.coelhotechne.detection_system.installation.domain.Installation;
 import com.coelhotechne.detection_system.sensor.domain.Sensor;
 import com.coelhotechne.detection_system.zone.domain.Zone;
-import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.format.annotation.DateTimeFormat;
 import tools.jackson.databind.PropertyNamingStrategies;
-import tools.jackson.databind.annotation.JsonDeserialize;
 import tools.jackson.databind.annotation.JsonNaming;
-import tools.jackson.databind.annotation.JsonSerialize;
-import tools.jackson.databind.ext.javatime.deser.LocalDateTimeDeserializer;
-import tools.jackson.databind.ext.javatime.ser.LocalDateTimeSerializer;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -21,15 +18,8 @@ import java.util.UUID;
 public record SensorRequest(
         @NotBlank
         String name,
-        boolean status,
+        boolean sensorStatus,
         @NotNull
-        @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
-        @JsonSerialize(using = LocalDateTimeSerializer.class)
-        @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-        @JsonFormat(
-                pattern = "dd/MM/yyyy HH:mm:ss",
-                shape = JsonFormat.Shape.STRING
-        )
         LocalDateTime activationTime,
         @NotNull
         BigDecimal memoryUsed,
@@ -37,20 +27,25 @@ public record SensorRequest(
         BigDecimal dataTransferValue,
         @NotBlank
         String dataDescription,
-        LocalDateTime installationDate,
-        UUID zoneUUID
+        @NotNull
+        Installation installation,
+        @NotNull
+        UUID zoneUUID,
+        @Valid
+        PowerSupply powerSupply
 
 ) {
     public static Sensor toEntity(SensorRequest request, Zone zone) {
         return new Sensor(
                 request.name(),
-                request.status(),
+                request.sensorStatus(),
                 request.activationTime(),
                 request.memoryUsed(),
                 request.dataTransferValue(),
                 request.dataDescription(),
-                request.installationDate(),
-                zone
-        );
+                request.installation(),
+                zone,
+                request.powerSupply()
+                );
     }
 }
