d9d2ea1a059c55152fde56bb23ecc0b1e95aa630
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-02 11:34:44 -0300

    updated: SensorService version:v1 project:detection-system date: 02092026

--- src/main/java/com/coelhotechne/detection_system/sensor/application/SensorService.java
+++ src/main/java/com/coelhotechne/detection_system/sensor/application/SensorService.java
@@ -3,6 +3,8 @@ package com.coelhotechne.detection_system.sensor.application;
 import com.coelhotechne.detection_system.sensor.api.dto.SensorRequest;
 import com.coelhotechne.detection_system.sensor.api.dto.SensorResponse;
 import com.coelhotechne.detection_system.sensor.api.dto.SensorTelemetryPayload;
+import com.coelhotechne.detection_system.sensor.domain.enums.SensorStatus;
+import com.coelhotechne.detection_system.sensor.domain.sensorevent.SensorEvent;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +16,6 @@ public interface SensorService {
     SensorResponse createSensor(SensorRequest sensorRequest);
     SensorResponse updateSensor(UUID uuid, SensorRequest sensorRequest);
     SensorResponse deleteSensor(UUID uuid);
-    void applyTelemetry(String zoneName, String sensorName, SensorTelemetryPayload payload,Boolean sensorStatus);
+    void applyTelemetry(String zoneName, String sensorName, SensorTelemetryPayload payload);
     void sendCommand(UUID sensorId, String action);
 }
