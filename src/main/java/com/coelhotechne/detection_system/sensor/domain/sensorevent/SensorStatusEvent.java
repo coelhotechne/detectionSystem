0feed8c7dc55cab1be86e2aab498ba574dd9aee0
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-02 11:32:20 -0300

    created: SensorStatusEvent version:v1 project:detection-system date: 02092026

---
+++ src/main/java/com/coelhotechne/detection_system/sensor/domain/sensorevent/SensorStatusEvent.java
@@ -0,0 +1,15 @@
+package com.coelhotechne.detection_system.sensor.domain.sensorevent;
+
+import com.coelhotechne.detection_system.sensor.domain.enums.SensorStatus;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public record SensorStatusEvent (
+        UUID eventId,
+        UUID sensorId,
+        SensorStatus previousStatus,
+        SensorStatus newStatus,
+        Instant occurredAt
+)implements SensorEvent{
+}
