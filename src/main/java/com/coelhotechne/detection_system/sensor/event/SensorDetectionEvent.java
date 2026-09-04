69c672f38b6cdae2f15ecdf09e6c636525ce2374
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-04 17:07:16 -0300

    updated: SensorDetectionEvent version:v1 project:detection-system date: 04092026

--- src/main/java/com/coelhotechne/detection_system/sensor/event/SensorDetectionEvent.java
+++ src/main/java/com/coelhotechne/detection_system/sensor/event/SensorDetectionEvent.java
@@ -1,5 +1,7 @@
 package com.coelhotechne.detection_system.sensor.event;
 
+import com.coelhotechne.detection_system.sensor.domain.payload.SensorDetectionPayload;
+
 import java.time.Instant;
 import java.util.UUID;
 
