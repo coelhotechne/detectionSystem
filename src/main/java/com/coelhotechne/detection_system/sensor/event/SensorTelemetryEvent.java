2b1942bed2fbf787936fc46992b1a7c15a2c98e9
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-04 17:05:43 -0300

    updated: SensorTelemetryEvent version:v1 project:detection-system date: 04092026

--- src/main/java/com/coelhotechne/detection_system/sensor/event/SensorTelemetryEvent.java
+++ src/main/java/com/coelhotechne/detection_system/sensor/event/SensorTelemetryEvent.java
@@ -1,6 +1,6 @@
 package com.coelhotechne.detection_system.sensor.event;
 
-import com.coelhotechne.detection_system.sensor.api.dto.SensorTelemetryPayload;
+import com.coelhotechne.detection_system.sensor.domain.payload.SensorTelemetryPayload;
 
 import java.time.Instant;
 import java.util.UUID;
