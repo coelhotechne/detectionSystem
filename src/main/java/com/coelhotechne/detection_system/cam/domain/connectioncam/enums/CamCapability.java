8baf94b8876b281ab2e65a5b4dcc48c4763efa31
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-06 12:22:48 -0300

    created: CamCapability version:v1 project:detection-system date: 06092026

---
+++ src/main/java/com/coelhotechne/detection_system/cam/domain/connectioncam/enums/CamCapability.java
@@ -0,0 +1,9 @@
+package com.coelhotechne.detection_system.cam.domain.connectioncam.enums;
+
+public enum CamCapability {
+    PTZ_CONTROL,
+    TWO_WAY_AUDIO,
+    NIGHT_VISION,
+    ONBOARD_MOTION_DETECTION,
+    ONBOARD_RECORDING
+}
