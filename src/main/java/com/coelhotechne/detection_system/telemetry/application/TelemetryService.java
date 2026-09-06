c7fe37ffb6259c8b61fddb14cba4f5d0d1fa6de5
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-06 10:55:05 -0300

    updated: Telemetry version:v1 project:detection-system date: 06092026

--- src/main/java/com/coelhotechne/detection_system/telemetry/application/TelemetryService.java
+++ src/main/java/com/coelhotechne/detection_system/telemetry/application/TelemetryService.java
@@ -3,13 +3,16 @@ package com.coelhotechne.detection_system.telemetry.application;
 import com.coelhotechne.detection_system.telemetry.api.dto.TelemetryRequest;
 import com.coelhotechne.detection_system.telemetry.api.dto.TelemetryResponse;
 import com.coelhotechne.detection_system.telemetry.domain.Telemetry;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface TelemetryService {
     Telemetry requireTelemetry(UUID telemetryId);
-    List<TelemetryResponse> findTelemetryList();
+    Page<TelemetryResponse> findTelemetryPageList(Pageable pageable);
+    List<TelemetryResponse> findAllTelemetryList();
     TelemetryResponse findTelemetryId(UUID uuid);
     TelemetryResponse createTelemetry(TelemetryRequest telemetryRequest);
     TelemetryResponse updateTelemetry(UUID uuid,TelemetryRequest telemetryRequest);
@@ -17,4 +20,5 @@ public interface TelemetryService {
     TelemetryResponse findTelemetryWithSensor(UUID telemetryId,UUID sensorId);
     TelemetryResponse findTelemetryWithZone(UUID telemetryId,UUID zoneid);
     TelemetryResponse findTelemetryWithZoneAndSensor(UUID telemetryId,UUID zoneid, UUID sensorId);
+    List<TelemetryResponse> findLatestBySensor(UUID sensorId);
 }
