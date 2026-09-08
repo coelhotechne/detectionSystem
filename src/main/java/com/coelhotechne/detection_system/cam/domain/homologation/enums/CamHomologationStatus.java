27a565cf50dee51703c073420de95a027286caf8
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-08 10:33:23 -0300

    created: CamHomologationStatus version:v1 project:detection-system date: 08092026

---
+++ src/main/java/com/coelhotechne/detection_system/cam/domain/homologation/enums/CamHomologationStatus.java
@@ -0,0 +1,9 @@
+package com.coelhotechne.detection_system.cam.domain.homologation.enums;
+
+public enum CamHomologationStatus {
+    PENDING_TEST,
+    TEST_FAILED,
+    PENDING_APPROVAL,
+    APPROVED,
+    REJECTED
+}
