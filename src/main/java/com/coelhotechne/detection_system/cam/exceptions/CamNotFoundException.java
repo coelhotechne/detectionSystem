4853071587481458ef5fd59567f2a89f501c94e3
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-08 10:04:01 -0300

    updated: CamNotFoundException version:v1 project:detection-system date: 08092026

--- src/main/java/com/coelhotechne/detection_system/cam/exceptions/CamNotFoundException.java
+++ src/main/java/com/coelhotechne/detection_system/cam/exceptions/CamNotFoundException.java
@@ -4,18 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.ErrorResponseException;
 
-public class CamFixedNotFoundException extends ErrorResponseException {
+public class CamNotFoundException extends ErrorResponseException {
 
     private final String camId;
 
-    public CamFixedNotFoundException(String camId,String cause){
+    public CamNotFoundException(String camId, String cause){
         super(HttpStatus.NOT_FOUND,buildProblemDetail(camId,cause),null);
         this.camId=camId;
     }
 
     private static ProblemDetail buildProblemDetail(String camId,String cause){
         ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,cause);
-        pd.setTitle("Id Not Found");
+        pd.setTitle("Cam Id Not Found");
         pd.setProperty("Id",camId);
 
         return pd;
