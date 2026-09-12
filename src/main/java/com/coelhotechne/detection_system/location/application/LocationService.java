35fcfd5d70c7c49320d27f76d4666595b3354e79
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-12 01:26:11 -0300

    updated: LocationService version:v1 project:detection-system date: 12092026

--- src/main/java/com/coelhotechne/detection_system/location/application/LocationService.java
+++ src/main/java/com/coelhotechne/detection_system/location/application/LocationService.java
@@ -2,11 +2,13 @@ package com.coelhotechne.detection_system.location.application;
 
 import com.coelhotechne.detection_system.location.api.dto.LocationRequest;
 import com.coelhotechne.detection_system.location.api.dto.LocationResponse;
+import com.coelhotechne.detection_system.location.domain.Location;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface LocationService {
+    Location requireLocation(UUID locationId);
     List<LocationResponse>findLocationList();
     LocationResponse findLocationId(UUID uuid);
     LocationResponse createLocation(LocationRequest locationRequest);
