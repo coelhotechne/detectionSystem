4a92463640fde7aae85328800c18f4bf9e36e1ba
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-18 19:49:30 -0300

    updated: SensorService version:v1 project:detection-system date: 18092026

--- src/main/java/com/coelhotechne/detection_system/sensor/application/SensorService.java
+++ src/main/java/com/coelhotechne/detection_system/sensor/application/SensorService.java
@@ -1,20 +1,30 @@
 package com.coelhotechne.detection_system.sensor.application;
 
+import com.coelhotechne.detection_system.sensor.api.dto.SensorPatchRequest;
+import com.coelhotechne.detection_system.sensor.api.dto.SensorReplaceRequest;
 import com.coelhotechne.detection_system.sensor.api.dto.SensorRequest;
 import com.coelhotechne.detection_system.sensor.api.dto.SensorResponse;
 import com.coelhotechne.detection_system.sensor.domain.Sensor;
+import com.coelhotechne.detection_system.sensor.domain.enums.SensorCommand;
 import com.coelhotechne.detection_system.sensor.event.SensorEvent;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface SensorService {
+
     Sensor requireSensor(UUID sensorId);
-    List<SensorResponse> findSensorList();
+    Page<SensorResponse> findSensorPageList(Pageable pageable);
     SensorResponse findSensorId(UUID uuid);
     SensorResponse createSensor(SensorRequest sensorRequest);
-    SensorResponse updateSensor(UUID uuid, SensorRequest sensorRequest);
+    SensorResponse replaceSensor(UUID uuid, SensorReplaceRequest request, Long expectedVersion);
+    SensorResponse patchSensor(UUID uuid, SensorPatchRequest patch, Long expectedVersion);
     SensorResponse deleteSensor(UUID uuid);
+
+    //::::::::::::::::::::Intern processing::::::::::::::::::::::::::::::::::
+    SensorResponse requestMaintenance(UUID uuid);
+    SensorResponse clearMaintenance(UUID uuid);
     void process(SensorEvent event);
-    void sendCommand(UUID sensorId, String action);
+    void sendCommand(UUID sensorId, SensorCommand sensorCommand);
 }
