7b24962ee6af5d1897bb332b17e43a1875efe4a5
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-21 22:48:58 -0300

    updated: SensorWithoutZoneException version:v1 project:detection-system date: 21092026

--- src/main/java/com/coelhotechne/detection_system/sensor/exceptions/SensorWithoutZoneException.java
+++ src/main/java/com/coelhotechne/detection_system/sensor/exceptions/SensorWithoutZoneException.java
@@ -1,9 +1,23 @@
 package com.coelhotechne.detection_system.sensor.exceptions;
 
-public class SensorWithoutZoneException extends RuntimeException {
-    private final String id;
-    public SensorWithoutZoneException(String id, String message) {
-        super(message);
-        this.id = id;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.ErrorResponseException;
+
+@Getter
+public class SensorWithoutZoneException extends ErrorResponseException {
+    private final String sensorId;
+
+    public SensorWithoutZoneException(String sensorId, String reason) {
+        super(HttpStatus.CONFLICT, buildProblemDetail(sensorId, reason), null);
+        this.sensorId = sensorId;
+    }
+
+    private static ProblemDetail buildProblemDetail(String sensorId, String reason) {
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, reason);
+        pd.setTitle("Sensor sem zona");
+        pd.setProperty("sensorId", sensorId);
+        return pd;
     }
 }
