5fad914580a8739a8b57950ece6c2d3585520c12
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-25 18:54:56 -0300

    updated: RtspException version:v1 project:detection-system date: 25092026

--- src/main/java/com/coelhotechne/detection_system/cam/exceptions/rtsp/RtspException.java
+++ src/main/java/com/coelhotechne/detection_system/cam/exceptions/rtsp/RtspException.java
@@ -3,8 +3,8 @@ package com.coelhotechne.detection_system.cam.exceptions.rtsp;
 public class RtspException extends RuntimeException {
     private final int statusCode;
 
-    public RtspException(int statusCode,String message) {
-        super(message);
+    public RtspException(int statusCode,String cause) {
+        super(cause);
         this.statusCode=statusCode;
     }
 
