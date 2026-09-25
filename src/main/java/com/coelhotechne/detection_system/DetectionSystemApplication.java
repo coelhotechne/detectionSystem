f179176ee53cc29209164bca39ff7a61ee459494
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2026-09-25 20:09:01 -0300

    updated: DetectionSystemApplication version:v1 project:detection-system date: 25092026

--- src/main/java/com/coelhotechne/detection_system/DetectionSystemApplication.java
+++ src/main/java/com/coelhotechne/detection_system/DetectionSystemApplication.java
@@ -2,8 +2,10 @@ package com.coelhotechne.detection_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DetectionSystemApplication {
 
 	public static void main(String[] args) {
